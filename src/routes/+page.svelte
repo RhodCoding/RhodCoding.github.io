18a2c8bc2fc7da14809817d599a23d5986f7e651
RhodCoding <joewee321@gmail.com>
2024-10-07 19:43:29 +0800

    Add School, Skill and Hobby content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,5 +15,38 @@
 
 <div class="container-two">
 
+    <div class="school">
+        <div class="school-two">
+         <img src="/assets/school.png" alt="School">
+         <h1>School?</h1>
+        </div>
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Eaque reiciendis quas laborum placeat perspiciatis laudantium in, impedit, 
+            ab rem sapiente esse nemo cumque repellat, incidunt obcaecati 
+            vel cum fuga delectus?</p>
+    </div>
+
+    <div class="skill">
+        <div class="skill-two">
+            <img src="/assets/skill.png" alt="Skill">
+            <h1>Skills?</h1>
+           </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+               Eaque reiciendis quas laborum placeat perspiciatis laudantium in, impedit, 
+               ab rem sapiente esse nemo cumque repellat, incidunt obcaecati 
+               vel cum fuga delectus?</p>
+    </div>
+
+    <div class="hobby">
+        <div class="hobby-two">
+            <img src="/assets/hobby.png" alt="Hobby">
+            <h1>Hobby?</h1>
+           </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+               Eaque reiciendis quas laborum placeat perspiciatis laudantium in, impedit, 
+               ab rem sapiente esse nemo cumque repellat, incidunt obcaecati 
+               vel cum fuga delectus?</p>
+    </div>
+
 </div>
 </section>
