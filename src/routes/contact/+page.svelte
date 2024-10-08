d46ebaadeb11fdf84a8d6c89e64117ac00ef4126
RhodCoding <joewee321@gmail.com>
2024-10-09 00:11:55 +0800

    Contact is Done

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -3,6 +3,23 @@
         <h1>Contact</h1>
     </div>
     <div class="social">
-        <img src="" alt="">
+
+       <div class="social2">
+          <img src="/assets/facebook.png" alt="Facebook"> 
+          <p>Rhodelito Pitalbo Pingal</p>
+       </div>
+
+       <div class="social2">
+          <img src="/assets/messenger.png" alt="Messenger">
+          <p>Rhodelito Pingal</p>
+       </div>
+
+       <div class="social2">
+          <img src="/assets/gmail.png" alt="Gmail">
+          <p>rhodpingal@gmail.com</p>
+       </div>
+
+       <img src="/assets/contact-img.png" alt="" class="contact-img">
+
     </div>
 </div>
